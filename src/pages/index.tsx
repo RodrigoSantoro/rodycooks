b5892692b30829fe09ef4901cbbd5815393610f8
Rodrigo Santoro <git@rodrigoss.com>
2024-02-21 21:49:19 -0800

    add newtab icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
     type MRT_ColumnDef,
 } from 'material-react-table'
 import { useMemo } from 'react'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 
 interface Props {
     dishes: Dishes
@@ -30,8 +31,13 @@ export default function HomePage({ dishes }: Props) {
                 header: 'Name',
                 enableHiding: false,
                 Cell: ({ cell }) => (
-                    <Link href={`recipes/${cell.row.original.url}`}>
+                    <Link
+                        href={`recipes/${cell.row.original.url}`}
+                        target="_blank"
+                        style={{ display: 'flex', alignItems: 'center' }}
+                    >
                         {cell.getValue<string>()}
+                        <OpenInNewIcon sx={{ pl: 1, fontSize: 12 }} />
                     </Link>
                 ),
             },
